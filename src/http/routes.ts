3016aa352384020f7150522e59f726a0176d5d3b
jefferson.willian <jefferson.lsilva@me.com.br>
2024-06-07 09:26:52 -0300

    gets

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,8 +1,24 @@
 import { FastifyInstance } from 'fastify'
 import { register } from './controllers/register'
 import { registerCheck } from './controllers/check'
+import { prisma } from '@/lib/prisma'
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', register)
   app.post('/check', registerCheck)
+
+  app.get('/allUsers', async () => {
+
+    const users = await prisma.user.findMany()
+
+    return { users }
+
+  })
+
+  app.get('/allChecks', async () => {
+
+    const checks = await prisma.checkIn.findMany()
+
+    return { checks }
+  })
 }
